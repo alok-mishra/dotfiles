6b3cbb9f072998113ae75ded1857df7d1194ff2f
Alok Mishra <alok@alokmishra.com>
2023-07-25 02:35:56 -0400

    vscode and git aliases, lazygit config, wezterm os check conditions

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -19,9 +19,13 @@ end
 if string.find(wezterm.target_triple, "linux") then
 	print("linux")
 	config.window_decorations = "RESIZE" -- hides window title
+
+	config.font = wezterm.font_with_fallback({ "FiraCode Nerd Font", "FiraCode NFM" })
 elseif string.find(wezterm.target_triple, "windows") then
 	print("windows")
-	config.default_prog = { "bash", "--login", "-i" } -- setbash
+	config.default_prog = { "bash", "--login", "-i" } -- set shell to bash
+
+	config.font = wezterm.font_with_fallback({ "Cascadia Code", "FiraCode NFM" })
 end
 
 config.check_for_updates = false
@@ -30,23 +34,29 @@ config.initial_cols = 160
 
 config.hide_tab_bar_if_only_one_tab = true
 config.window_close_confirmation = "NeverPrompt"
-
-config.font =
-	wezterm.font_with_fallback({ "Cascadia Code", "FiraCode Nerd Font", "Inconsolata Nerd Font", "FiraCode NFM" })
 config.font_size = 10.0
 
 config.color_scheme = "GruvboxDark"
 -- config.color_scheme = "Catppuccin Mocha"
 
+local function randomBackground(dir)
+	local files = wezterm.read_dir(dir)
+	local file = files[math.random(1, #files)]
+	if file:match("%.jpe?g$") then
+		return file
+	else
+		return wezterm.home_dir .. "/.config/wezterm/background.jpg"
+	end
+end
+
 config.background = {
 	{
-		source = { File = wezterm.home_dir .. "/.config/wezterm/background.jpg" },
+
+		source = { File = randomBackground(wezterm.home_dir .. "/sync/settings/backgrounds") },
 		-- opacity = 0.88,
 		-- attachment = { Parallax = 0.2 },
-		hsb = { hue = 1.0, saturation = 1.0, brightness = 0.2 },
+		hsb = { hue = 1.0, saturation = 1.0, brightness = 0.06 },
 	},
 }
-
--- wezterm.get_builtin_color_schemes()
 
 return config
